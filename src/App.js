1717c0a97ab4a4165c2bdf1ea94d46032372457e
ZiedAllouen <allouen.mohamed@EPISOUSSE.COM.TN>
2022-07-13 10:31:32 +0100

    v1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import {Container,AppBar,Typography,Grow,Grid} from '@material-ui/core';
+import cm from './images/cm.jpg'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Container maxWidth="lg">
+      <AppBar position='static' color='inherit'>
+        <Typography variant="h2" align='center'> Memories</Typography>
+        <img src={cm} alt="memories" heigth="60"/>
+      </AppBar>
+      <Grid in>
+        <Container>
+          <Grid container justify='space-between' alignItems='stretch' spacing={3}>
+
+          </Grid>
+        </Container>
+      </Grid>
+    </Container>
   )
 }
 
