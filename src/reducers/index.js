b2e2b68b89ff0728c57f987841703b557e38ceab
ZiedAllouen <allouen.mohamed@EPISOUSSE.COM.TN>
2022-11-07 10:08:57 +0100

    ces

--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import posts from './posts';
-import auth from './auth';
-import items from'./items';
-
-export const reducers = combineReducers({ posts, auth, items });
